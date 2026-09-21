1d87c21bf184885d4584072f5a8d6a9d8c781472
Till Harbaum <till@harbaum.org>
2026-09-21 11:33:25 +0200

    Fix vertical artifacts on OSD

--- src/misc/osd_u8g2.v
+++ src/misc/osd_u8g2.v
@@ -24,24 +24,36 @@ module osd_u8g2 (
   output [5:0] b_out
 );
 
+// the OSD is inserted behind the scan doubler and thus deals with
+// the higher pixel clock and twice the lines
+localparam HCNT_BITS = 12;   
+localparam VCNT_BITS = 10;      
+
 // OSD is enabled and visible
 reg enabled;
 
 // -------------------------- OSD painting -------------------------------
 
 reg vsD, hsD;
-reg [11:0] hcnt;    // signal ranges 0..1023
-reg [11:0] hcntL;
-reg [9:0] vcnt;    // signal ranges 0..626
-reg [9:0] vcntL;
+reg [HCNT_BITS-1:0] hcnt;       // signal ranges 0..1023
+reg [HCNT_BITS-1:0] scr_width;
+reg [VCNT_BITS-1:0] vcnt;       // signal ranges 0..626
+reg [VCNT_BITS-1:0] scr_height;
 
 // OSD is active on current pixel, the shadow is active or the text area is active
-wire active, sactive, tactive;
-   
+reg  active, tactive;
+
+// disabling the shadow saves some LUTs
+`ifndef OSD_NO_SHADOW   
+reg sactive;
+`else
+wire sactive = 1'b0;
+`endif   
+
 wire	   osd_pix;  
 wire [5:0] osd_pix_col;
 
-// background is darker where "shadow" is active
+// background shines through less where "shadow" is active
 wire [5:0] osd_r = (tactive && osd_pix)?osd_pix_col:sactive?{4'b0000, r_in[5:4]}:{3'b000, r_in[5:3]};
 wire [5:0] osd_g = (tactive && osd_pix)?osd_pix_col:sactive?{4'b0000, g_in[5:4]}:{3'b000, g_in[5:3]};
 wire [5:0] osd_b = (tactive && osd_pix)?osd_pix_col:sactive?{4'b0100, b_in[5:4]}:{3'b010, b_in[5:3]};  
@@ -55,27 +67,35 @@ assign b_out = !enabled?b_in:active?osd_b:sactive?{1'b0, b_in[5:1]}:b_in;
 `define BORDER 2
 `define SHADOW 4
 `define SCALE  2
-`define WIDTH 16   // OSD width in characters
-`define HEIGHT 8   // OSD height in characters
+`define WIDTH 16   // OSD width in 8 pixel wide characters
+`define HEIGHT 8   // OSD height in 8 pixel tall characters
 
-wire [11:0] hstart = (hcntL/2)-8*`WIDTH*`SCALE/2;
-wire [9:0]  vstart = (vcntL/2)-8*`HEIGHT*`SCALE/2;
+// make sure OSD is centered
+wire [HCNT_BITS-1:0] hstart = {1'b0, scr_width[HCNT_BITS-1:1]}-8*`WIDTH*`SCALE/2;
+wire [VCNT_BITS-1:0] vstart = {1'b0, scr_height[VCNT_BITS-1:1]}-8*`HEIGHT*`SCALE/2;
 
-// entire OSD area incl border
-wire	    hactive = hcnt >= hstart-`SCALE*`BORDER && hcnt < hstart+`SCALE*`BORDER+8*`WIDTH*`SCALE;
-wire	    vactive = vcnt >= vstart-`SCALE*`BORDER && vcnt < vstart+`SCALE*`BORDER+8*`HEIGHT*`SCALE;
-assign      active = hactive && vactive;  
+always @(posedge clk) begin
+   // entire OSD area incl border
+   active <= hcnt >= hstart-`SCALE*`BORDER-1 && 
+	     hcnt < hstart+`SCALE*`BORDER+8*`WIDTH*`SCALE-1 &&
+	     vcnt >= vstart-`SCALE*`BORDER && 
+	     vcnt < vstart+`SCALE*`BORDER+8*`HEIGHT*`SCALE;
+   
+   // text area of OSD
+   tactive <= hcnt >= hstart-1 && 
+	      hcnt < hstart+8*`WIDTH*`SCALE-1 &&
+	      vcnt >= vstart && 
+	      vcnt < vstart+8*`HEIGHT*`SCALE;
 
-// text area of OSD
-wire	    thactive = hcnt >= hstart && hcnt < hstart+8*`WIDTH*`SCALE;
-wire	    tvactive = vcnt >= vstart && vcnt < vstart+8*`HEIGHT*`SCALE;
-assign 	    tactive = thactive && tvactive;  
-
-// shadow area of OSD
-wire	    shactive = hcnt >= hstart-`SCALE*`BORDER+`SCALE*`SHADOW && hcnt < hstart+`SCALE*`BORDER+`SCALE*`SHADOW+8*`WIDTH*`SCALE;
-wire	    svactive = vcnt >= vstart-`SCALE*`BORDER+`SCALE*`SHADOW && vcnt < vstart+`SCALE*`BORDER+`SCALE*`SHADOW+8*`HEIGHT*`SCALE;
-assign	    sactive = shactive && svactive;  
-
+`ifndef OSD_NO_SHADOW   
+   // shadow area of OSD
+   sactive <= hcnt >= hstart-`SCALE*`BORDER+`SCALE*`SHADOW-1 &&
+	      hcnt < hstart+`SCALE*`BORDER+`SCALE*`SHADOW+8*`WIDTH*`SCALE-1 &&
+	      vcnt >= vstart-`SCALE*`BORDER+`SCALE*`SHADOW &&
+	      vcnt < vstart+`SCALE*`BORDER+`SCALE*`SHADOW+8*`HEIGHT*`SCALE;
+`endif
+end
+   
 // 1024 bytes = 8192 pixels = 128 x 64 pixels
 reg [7:0] buffer [1024];  
 
@@ -123,21 +143,20 @@ wire [6:0] vpix  = vcnt-vstart;  // vertical pixel position inside OSD
 reg [7:0] buffer_byte;
 assign osd_pix = buffer_byte[vpix[3:1]];
 always @(posedge clk)
-   buffer_byte <= buffer[{ vpix[6:4], hpixD[7:1] }];
+    buffer_byte <= buffer[{ vpix[6:4], hpixD[7:1] }];
    
 assign osd_pix_col = 6'd63;
 
 // -------------------------- video signal analysis -------------------------
    
 // analyze video timing to determine center of screen
-
 always @(posedge clk) begin
    // ---- hsync processing -----
    hsD <= hs;
 
    // end of hsync, rising edge
    if(hs && !hsD) begin
-      hcntL <= hcnt;
+      scr_width <= hcnt;
       hcnt <= 0;
    end else
      hcnt <= hcnt + 12'd1;
@@ -147,7 +166,7 @@ always @(posedge clk) begin
       vsD <= vs;
       // begin of vsync, falling edge
       if(!vs && vsD) begin
-         vcntL <= vcnt;
+         scr_height <= vcnt;
          vcnt <= 0;
       end else
         vcnt <= vcnt + 10'd1;
